abe4d613da5e2f3e1e07232d68de6ede4b89d86f
hhoutmann <hhoutmann@gmail.com>
2019-01-12 19:19:27 +0100

    TFIDF

--- recommandation/Script.py
+++ recommandation/Script.py
@@ -11,6 +11,7 @@ from nltk import PorterStemmer
 from nltk.corpus import stopwords
 import operator
 from collections import Counter
+import time
 
 cachedStopWords = stopwords.words("french") + stopwords.words("english")
 conn = psycopg2.connect("dbname='django123' user='postgres' host='localhost' password=''")
@@ -20,15 +21,17 @@ def getWords(text):
 def getKey(item):
     return item[1]
 
-def analyseFile(filename, serie):
-
-    subs = pysrt.open(filename, encoding='iso-8859-1')
+def analyseFile(liste_episode, serie):
     list = []
     string = ''
-    for i in range(len(subs)):
-        for j in getWords(subs[i].text):
-            list.append(j.lower())
-            string = string + ' ' + j
+
+    for episode in liste_episode:
+        subs = pysrt.open(episode, encoding='iso-8859-1')
+
+        for i in range(len(subs)):
+            for j in getWords(subs[i].text):
+                list.append(j.lower())
+                string = string + ' ' + j
 
     filtered_words = [word for word in list if word not in cachedStopWords]
     d = Counter(' '.join(filtered_words).split())
@@ -40,42 +43,52 @@ def analyseFile(filename, serie):
     for x in sorted_d:
         list.add(x)
     list_sorted = sorted(list, key=getKey)
+    for word in list_sorted:
+        if len(word[0]) > 2:
+            try:
+                    cur.execute(
+                        "INSERT INTO recommandation_keywords (key) VALUES ('{}') returning id".format(word[0]))
+                    key_id = cur.fetchone()[0]
+                    conn.commit()
+            except Exception as e:
+                conn.rollback()
+                cur.execute("SELECT k.id from recommandation_keywords as k where k.key='{}'".format(word[0]))
+                key_id = cur.fetchone()[0]
 
-    for word in list_sorted[-100:]:
-        try:
-            cur.execute(
-                "INSERT INTO recommandation_keywords (key) VALUES ('{}') returning id".format(word[0]))
-            key_id = cur.fetchone()[0]
-
-        except:
-            conn.rollback()
-            cur.execute("SELECT k.id from recommandation_keywords as k where k.key='{}'".format(word[0]))
-            key_id = cur.fetchone()[0]
-        cur.execute("INSERT INTO recommandation_posting (number, keywords_id, series_id) VALUES ('{0}','{1}','{2}')".format(word[1], key_id, serie))
-        conn.commit()
+            cur.execute("INSERT INTO recommandation_posting (number, keywords_id, series_id) VALUES ('{0}','{1}','{2}')".format(word[1], key_id, serie))
+            conn.commit()
 
 def walk_sub():
     for root in os.scandir("/home/hadrien/Bureau/sous-titres"):
+        start = time.time()
         cur = conn.cursor()
         cur.execute("INSERT INTO recommandation_series (name) VALUES ('{}') returning id".format(root.name))
         conn.commit()
         serie_id = cur.fetchone()[0]
-        
+        liste_episode = []
         for files in os.scandir(root):
             if str(files.name)[-4:] == '.zip':
                 pass
             else:
-
-                analyseFile("/home/hadrien/Bureau/sous-titres/" + root.name + '/' + files.name, serie=serie_id)
+                liste_episode.append("/home/hadrien/Bureau/sous-titres/" + root.name + '/' + files.name,)
+        analyseFile(liste_episode, serie=serie_id)
+        end = time.time()
+        print('walk ', end - start)
     conn.close()
-import time
+
+
+
+
+
+
+
 import nltk
 start = time.time()
 #nltk.download('stopwords')
 walk_sub()
 
 end = time.time()
-print(end - start)
+print('total ',end - start)
 
 
 
--- recommandation/searchTFIDF.py
+++ recommandation/searchTFIDF.py
@@ -1,28 +1,24 @@
+import math
+
 import psycopg2
 from sklearn.feature_extraction.text import CountVectorizer
 
-conn = psycopg2.connect("dbname='django123' user='postgres' host='localhost' password=''")
 
 
 
-cur = conn.cursor()
-
-cur.execute("SELECT k.key, p.number FROM recommandation_keywords as k, recommandation_posting as p, recommandation_series as s WHERE s.name = 'futurama' AND p.series_id=s.id AND p.keywords_id=k.id")
-corpus = cur.fetchall()
-D = dict(corpus)
-print(len(D))
-
-def calculTf(corpus):
+def calculTf(word, serie_pk):
     tfDict = dict()
     lenght = 0
+    cur.execute(
+        "SELECT k.key, p.number FROM recommandation_keywords as k, recommandation_posting as p, recommandation_series as s WHERE s.id = '{}' AND p.series_id=s.id AND p.keywords_id=k.id".format(serie_pk))
+    D = dict(cur.fetchall())
     for key, value in D.items():
         lenght = lenght + value
     for key, value in D.items():
         tfDict[key] = value / lenght
-    print(len(tfDict))
+    return float(tfDict[word])
 
 
-#calculTf(D)
 
 def lenCollection():
     cur.execute(
@@ -32,11 +28,26 @@ def lenCollection():
 
 def idf(word):
     cur.execute(
-        "SELECT * FROM recommandation_keywords as k, recommandation_posting as p, recommandation_series as s WHERE k.key = 'guerre' AND p.series_id=s.id AND p.keywords_id=k.id")
-    corpus = cur.fetchall()
-    print(corpus)
+        "SELECT count(*) FROM recommandation_keywords as k, recommandation_posting as p, recommandation_series as s WHERE k.key = '{}' AND p.series_id=s.id AND p.keywords_id=k.id".format(word))
+    documentWithTermCount = cur.fetchall()
+    return float(math.log10(lenCollection() / documentWithTermCount[0][0]))
 
-idf('toto')
+def tfIdf(word, liste_series):
+    for serie in liste_series:
+        tf = calculTf(word, serie[0])
+        idff = idf(word)
+        cur.execute("SELECT s.name FROM recommandation_series as s WHERE s.id ='{}'".format(serie[0]))
+        serie_name = cur.fetchall()
+        print(serie_name[0][0], tf * idff)
 
 
+conn = psycopg2.connect("dbname='django123' user='postgres' host='localhost' password=''")
+
+
+cur = conn.cursor()
+
+cur.execute("SELECT s.id FROM recommandation_keywords as k, recommandation_posting as p, recommandation_series as s WHERE  p.series_id=s.id AND p.keywords_id=k.id AND k.key = 'sex'")
+liste_series = cur.fetchall()
+
+tfIdf('sex', liste_series)
 
