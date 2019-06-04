5f4cb78a80539bcbcce84bbac27b2072587ce40a
hadrien <hhoutmann@gmail.com>
2019-06-04 16:57:15 +0200

    Protection des URLS, et nettoyage du code

--- recommandation/tasks.py
+++ recommandation/tasks.py
@@ -15,7 +15,6 @@ from PTUT.settings import DATABASES, API_KEY, STATIC_URL, STATICFILES_DIRS
 from PTUT import HUEY, MEDIA_ROOT
 from django.core import management
 from recommandation.models import Series
-from recommandation.management.commands.loadsubtitles import walk_sub, insertInDatabase, read_srt_files
 
 cachedStopWords = stopwords.words("french") + stopwords.words("english")
 
@@ -25,17 +24,15 @@ conn = psycopg2.connect("dbname='{0}' user='{1}' host='{2}' password='{3}'".form
 																				   DATABASES['default']['PASSWORD']))
 
 
-
-
 @HUEY.task()
 def file_processing(filename):
 	cur = conn.cursor()
 	zip_obj = zipfile.ZipFile(MEDIA_ROOT[0] + filename, 'r')
 	zip_obj.extractall(MEDIA_ROOT[0])
 	zip_obj.close()
-	management.call_command('loadsubtitles', MEDIA_ROOT[0])
+	subs = walk_sub(MEDIA_ROOT[0])
 
-	subs = walk_sub(MEDIA_ROOT[0])  # Ne pas oublier le slash a la fin
+	serie_id = None
 	for key, value in subs.items():
 		text = read_srt_files(value)
 		serie_id = insertInDatabase(key, text['corpus'], text['lenCorpus'])
@@ -69,12 +66,104 @@ def file_processing(filename):
 	management.call_command('refreshMatViews')
 
 
+def getWords(text):
+	return re.findall('\w+', text)
 
 
+def getKey(item):
+	return item[1]
 
 
+def calculTf(corpus, maxi):
+	resultat = dict()
+	for word, number in corpus.items():
+		resultat[word] = (number, round(number / maxi, 4))
+	# print(sorted(resultat.items(), key=operator.itemgetter(1), reverse=True))
+	return resultat
 
 
+def maxNB(corpus):
+	return corpus[max(corpus, key=corpus.get)]
+
+
+def read_srt_files(listSrt):
+	corpus = collections.Counter()
+	for episode in listSrt:
+		subs = pysrt.open(episode, encoding='iso-8859-1')
+
+		stemmer = PorterStemmer()
+
+		tokens = nltk.word_tokenize(subs.text)
+
+		words = [stemmer.stem(unidecode.unidecode(w.lower())) for w in tokens if
+				 w.lower() not in cachedStopWords and len(w) > 2 and w.lower().isalpha()]
+		# words = [w.lower() for w in tokens if w.lower() not in cachedStopWords and len(w) > 2 and w.lower().isalpha()]
+
+		corpus.update(words)
+
+	maxi = maxNB(corpus)
+	corpusWithTf = calculTf(corpus, maxi)
+
+	return {'corpus': corpusWithTf, 'lenCorpus': maxi}
+
+
+def insertInDatabase(serieName, corpus, lenCorpus):
+	cur = conn.cursor()
+	cur.execute("UPDATE recommandation_series  SET max_keyword_nb='{}' WHERE name ='{}'".format(lenCorpus, serieName))
+	conn.commit()
+	cur.execute("SELECT * from recommandation_series where name = '{}'".format(serieName))
+	serie_id = cur.fetchone()[0]
+
+	for word, value in corpus.items():
+		# try:
+		key_id = cur.execute(
+			"INSERT INTO recommandation_keywords (key) VALUES ('{0}') ON CONFLICT (key) DO UPDATE set key='{0}' returning id".format(word))
+
+		key_id = cur.fetchone()[0]
+
+		cur.execute(
+			"INSERT INTO recommandation_posting (number, keywords_id, series_id, tf) VALUES ('{0}','{1}','{2}', '{3}')".format(
+				value[0], key_id, serie_id, value[1]))
+	conn.commit()
+
+	return serie_id
+
+
+def walk_sub(directory):
+	""" Parcours du dossier de sous titres retourne un dictionnaire"""
+	seriesPath = dict()
+	for root in os.scandir(directory):
+
+		listPath = []
+		for racine, dir, files in os.walk(directory + root.name):
+
+			for basename in files:
+				if fnmatch.fnmatch(basename, '*.srt'):
+					filename = os.path.join(racine, basename)
+					listPath.append(filename)
+
+			seriesPath[root.name] = listPath
+	return seriesPath
+
+
+def lenCollection():
+	cur = conn.cursor()
+	cur.execute(
+		"SELECT count(*) FROM recommandation_series as s")
+	lenCollection = cur.fetchall()
+	return lenCollection[0][0]
+
+
+def idf(word):
+	cur = conn.cursor()
+	cur.execute(
+		"SELECT count(s.id) FROM recommandation_keywords as k, recommandation_posting as p, recommandation_series as s WHERE k.key = '{}' AND p.series_id=s.id AND p.keywords_id=k.id".format(
+			word))
+	documentWithTermCount = cur.fetchall()
+	# print('len de la collection', lenCol)
+
+	result = math.log2(lenCollection() / documentWithTermCount[0][0])
+	return result
 
 
 @HUEY.task()
