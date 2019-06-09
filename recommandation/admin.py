2e27733e25764cdfe0b3f24d639217d5800cac51
hadrien <hhoutmann@gmail.com>
2019-06-09 11:08:09 +0200

    Admin

--- recommandation/admin.py
+++ recommandation/admin.py
@@ -146,7 +146,7 @@ class SerieFilter(AutocompleteFilter):
 
 @admin.register(Similarity)
 class SimilarityAdmin(admin.ModelAdmin):
-	search_fields = ['serie_realname']
+	search_fields = ['serie__realname']
 	list_display = ('serie', 'similar_to', 'score')
 	autocomplete_fields = ['serie']
 	# list_filter = ('serie',)
